8d9643e8994e286e0997ad97da14852f8a4cd3dd
Anna Cecília Roncalio <Anna.Roncalio@proway.treina>
2022-07-25 10:14:51 -0300

    [feat] upload all files

---
+++ lib/app/domain/entities/product_entity.dart
@@ -0,0 +1,16 @@
+class ProductEntity {
+  String name;
+  String brand;
+  String category;
+  double price;
+  int quantity;
+  
+  ProductEntity({
+    required this.name,
+    required this.brand,
+    required this.category,
+    required this.price,
+    required this.quantity,
+  });
+  
+}
